88868c25ab6b50a211347b87db44dba42a3c3b2f
mike <skrimmy@gmail.com>
2012-08-28 19:27:10 -0700

    updated tables

--- create_vic_tables.sql
+++ create_vic_tables.sql
@@ -1,10 +1,23 @@
 
+CREATE SEQUENCE VIC.vaccine_type_id_seq;
+
+CREATE TABLE VIC.vaccine_type (
+                vaccine_type_id INTEGER NOT NULL DEFAULT nextval('VIC.vaccine_type_id_seq'),
+                name VARCHAR(10) NOT NULL,
+                CONSTRAINT vaccine_type_id PRIMARY KEY (vaccine_type_id)
+);
+COMMENT ON TABLE VIC.vaccine_type IS 'Vaccine types (DTaP, Hib, MMR, etc)';
+
+
+ALTER SEQUENCE VIC.vaccine_type_id_seq OWNED BY VIC.vaccine_type.vaccine_type_id;
+
 CREATE SEQUENCE VIC.disease_id_seq;
 
 CREATE TABLE VIC.disease (
                 disease_id INTEGER NOT NULL DEFAULT nextval('VIC.disease_id_seq'),
+                vaccine_type_id INTEGER NOT NULL,
                 disease VARCHAR(50) NOT NULL,
-                CONSTRAINT disease_id PRIMARY KEY (disease_id)
+                CONSTRAINT disease_id PRIMARY KEY (disease_id, vaccine_type_id)
 );
 COMMENT ON TABLE VIC.disease IS 'diseases for which vaccinations are given for';
 COMMENT ON COLUMN VIC.disease.disease IS 'disease name';
@@ -19,14 +32,14 @@ CREATE TABLE VIC.vaccine_components (
                 vaccine_id INTEGER NOT NULL,
                 vaccine_component VARCHAR(100) NOT NULL,
                 dose NUMERIC(3,3) NOT NULL,
-                dose_scale VARCHAR(5) NOT NULL,
-                CONSTRAINT vaccine_component_id PRIMARY KEY (component_id)
+                dose_measurement VARCHAR(3) NOT NULL,
+                CONSTRAINT vaccine_component_id PRIMARY KEY (component_id, vaccine_id)
 );
 COMMENT ON TABLE VIC.vaccine_components IS 'components present in a vaccine';
 COMMENT ON COLUMN VIC.vaccine_components.component_id IS 'FK on vaccine.id';
 COMMENT ON COLUMN VIC.vaccine_components.vaccine_component IS 'name of vaccine component/antigen/adjuvant';
 COMMENT ON COLUMN VIC.vaccine_components.dose IS 'amount of antigen/adjuvant/etc present (mg or mcg)';
-COMMENT ON COLUMN VIC.vaccine_components.dose_scale IS 'mg, mcg, or ml dose scale';
+COMMENT ON COLUMN VIC.vaccine_components.dose_measurement IS 'mg, mcg, or ml dose scale';
 
 
 ALTER SEQUENCE VIC.vaccine_components_component_id_seq OWNED BY VIC.vaccine_components.component_id;
@@ -44,15 +57,16 @@ COMMENT ON COLUMN VIC.manufacturer.company_name IS 'vaccine manufacturer company
 
 ALTER SEQUENCE VIC.manufacturer_manufacturer_id_seq OWNED BY VIC.manufacturer.manufacturer_id;
 
-CREATE SEQUENCE VIC.vaccines_vaccine_id_seq;
+CREATE SEQUENCE VIC.vaccine_id_seq;
 
 CREATE TABLE VIC.vaccines (
-                vaccine_id INTEGER NOT NULL DEFAULT nextval('VIC.vaccines_vaccine_id_seq'),
-                disease_id INTEGER NOT NULL,
+                vaccine_id INTEGER NOT NULL DEFAULT nextval('VIC.vaccine_id_seq'),
                 manufacturer_id INTEGER NOT NULL,
+                vaccine_type_id INTEGER NOT NULL,
                 vaccine_name VARCHAR(50) NOT NULL,
                 dose NUMERIC(3,1) DEFAULT 0.5 NOT NULL,
-                CONSTRAINT vaccines_id PRIMARY KEY (vaccine_id, disease_id, manufacturer_id)
+                dose_measurement VARCHAR(3) DEFAULT 'ml' NOT NULL,
+                CONSTRAINT vaccines_id PRIMARY KEY (vaccine_id, manufacturer_id)
 );
 COMMENT ON TABLE VIC.vaccines IS 'vaccine base table';
 COMMENT ON COLUMN VIC.vaccines.manufacturer_id IS 'FK on manufacturer.id';
@@ -60,15 +74,22 @@ COMMENT ON COLUMN VIC.vaccines.vaccine_name IS 'Marketing name of vaccine produc
 COMMENT ON COLUMN VIC.vaccines.dose IS 'dose of vaccine to be administered ';
 
 
-ALTER SEQUENCE VIC.vaccines_vaccine_id_seq OWNED BY VIC.vaccines.vaccine_id;
+ALTER SEQUENCE VIC.vaccine_id_seq OWNED BY VIC.vaccines.vaccine_id;
 
 CREATE INDEX vaccines_idx
  ON VIC.vaccines
  ( manufacturer_id );
 
-ALTER TABLE VIC.vaccines ADD CONSTRAINT disease_vaccines_fk
-FOREIGN KEY (disease_id)
-REFERENCES VIC.disease (disease_id)
+ALTER TABLE VIC.vaccines ADD CONSTRAINT vaccine_type_vaccines_fk
+FOREIGN KEY (vaccine_type_id)
+REFERENCES VIC.vaccine_type (vaccine_type_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE VIC.disease ADD CONSTRAINT vaccine_type_disease_fk
+FOREIGN KEY (vaccine_type_id)
+REFERENCES VIC.vaccine_type (vaccine_type_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
