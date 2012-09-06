15c096ead42be3e04af130f965bb821020aaeaa0
mike <skrimmy@gmail.com>
2012-09-06 08:24:10 -0700

    Fixed Adacel vaccine_type_id, added Boostrix

--- initial data sets.sql
+++ initial data sets.sql
@@ -65,7 +65,8 @@ insert into vic.vaccines (vaccine_name, manufacturer_id, vaccine_type_id, combo_
 insert into vic.vaccines (vaccine_name, manufacturer_id, vaccine_type_id, combo_vaccine, dose) values ('Pediarix',2,3,'1',0.5);
 insert into vic.vaccines (vaccine_name, manufacturer_id, vaccine_type_id, combo_vaccine, dose) values ('Kinrix',2,4,'1',0.5);
 insert into vic.vaccines (vaccine_name, manufacturer_id, vaccine_type_id, combo_vaccine, dose) values ('Pentacel',1,5,'1',0.5);
-insert into vict.vaccines (vaccine_name, manufacturer_id, vaccine_type_id, combo_vaccine, dose) values ('Adacel',1,1,'1',0.5);
+insert into vic.vaccines (vaccine_name, manufacturer_id, vaccine_type_id, combo_vaccine, dose) values ('Adacel',1,21,'1',0.5);
+insert into vic.vaccines (vaccine_name, manufacturer_id, vaccine_type_id, combo_vaccine, dose) values ('Boostrix',1,21,'1',0.5);
 
 
 -- normalized_vaccine_components
